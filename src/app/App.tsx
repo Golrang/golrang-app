0998630fc8cfc1c6295a2b2e687c7a8cc531cf3e
amirHosseinKhademii <sourena2575@gmail.com>
2022-07-30 13:19:04 +0200

    Add new updated design system

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,13 @@
-import { FormG, InputG, TextareaG } from "golrang-design-system";
+import { CheckBox, Form, Input, Select, TextArea } from "golrang-design-system";
 import * as yup from "yup";
-type TFormProps = { name: string; user: string; age: number };
+
+type TFormProps = {
+  name: string;
+  user: string;
+  age: number;
+  isHired: boolean;
+  company: number;
+};
 type TKeyOf = keyof TFormProps;
 
 type TSchema<T> = {
@@ -11,19 +18,27 @@ const schema = yup.object<TSchema<TFormProps>>({
   name: yup.string().required(),
   user: yup.string().required(),
   age: yup.number().required(),
+  isHired: yup.boolean().required(),
+  company: yup.number().required(),
 });
 
 export const App = () => {
   const onSubmit = (state: TFormProps) => console.log(state);
   return (
-    <FormG<TFormProps>
+    <Form<TFormProps>
       className="  max-w-md mx-auto py-32"
       {...{ onSubmit, schema }}
     >
-      <InputG<TKeyOf> label="Name" name="name" />
-      <InputG<TKeyOf> label="Age" name="age" />
-      <TextareaG<TKeyOf> label="User" name="user" />
+      <Input<TKeyOf> label="Name" name="name" />
+      <Input<TKeyOf> label="Age" name="age" />
+      <TextArea<TKeyOf> label="User" name="user" />
+      <CheckBox<TKeyOf> label="Is Hired" name="isHired" />
+      <Select
+        label="Company"
+        name="company"
+        options={[{ label: "One", value: "1" }]}
+      />
       <button type="submit">Submit</button>
-    </FormG>
+    </Form>
   );
 };
