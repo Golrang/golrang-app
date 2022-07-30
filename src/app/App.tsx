b32f13751bcd2b74c596a3e2626d358432442c69
amirHosseinKhademii <sourena2575@gmail.com>
2022-07-30 12:38:46 +0200

    implement design css

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,29 @@
-import { Input } from "antd";
+import { FormG, InputG, TextareaG } from "golrang-design-system";
+import * as yup from "yup";
+type TFormProps = { name: string; user: string; age: number };
+type TKeyOf = keyof TFormProps;
+
+type TSchema<T> = {
+  [P in keyof T]: any;
+};
+
+const schema = yup.object<TSchema<TFormProps>>({
+  name: yup.string().required(),
+  user: yup.string().required(),
+  age: yup.number().required(),
+});
 
 export const App = () => {
+  const onSubmit = (state: TFormProps) => console.log(state);
   return (
-    <div className=" max-w-md mx-auto py-32">
-      <Input className="" />
-    </div>
+    <FormG<TFormProps>
+      className="  max-w-md mx-auto py-32"
+      {...{ onSubmit, schema }}
+    >
+      <InputG<TKeyOf> label="Name" name="name" />
+      <InputG<TKeyOf> label="Age" name="age" />
+      <TextareaG<TKeyOf> label="User" name="user" />
+      <button type="submit">Submit</button>
+    </FormG>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "app/App";
 import "styles/index.css";
 import "antd/dist/antd.css";
+import "golrang-design-system/dist/design.css";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
